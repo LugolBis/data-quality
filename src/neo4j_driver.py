e1d2bf36723aba71a56a12afa0711030dc220a24
LugolBis <ld.desmares@gmail.com>
2026-02-06 20:30:30 +0100

    Introduced Neo4j driver

--- src/neo4j_driver.py
+++ src/neo4j_driver.py
@@ -2,50 +2,48 @@ import os
 from typing import Any, LiteralString, Optional
 
 from dotenv import load_dotenv
-from neo4j import Driver, EagerResult, GraphDatabase, Query, RoutingControl
+from neo4j import GraphDatabase, Query, RoutingControl
 
 from utils import some
 
-load_dotenv()
 
-uri: Optional[str] = os.environ.get("URI")
-db_user: Optional[str] = os.environ.get("DB_USER")
-db_password: Optional[str] = os.environ.get("DB_PW")
-
-
-def run_query(
-    driver: Driver,
+def prepare_query(
     db_name: str,
     query: LiteralString,
     write_op: bool,
     parameters: Optional[dict[str, Any]] = None,
     timeout: Optional[float] = None,
-) -> Optional[EagerResult]:
-    query_prepared: Query = Query(text=query, metadata=parameters, timeout=timeout)
+) -> dict[str, Any]:
+    query_prepared = Query(text=query, timeout=timeout)
+    routing: RoutingControl
 
     if write_op:
-        driver.execute_query(
-            database_=db_name,
-            query_=query_prepared,
-            parameters_=parameters,
-            routing_=RoutingControl.WRITE,
-        )
-        return None
+        routing = RoutingControl.WRITE
     else:
-        return driver.execute_query(
-            database_=db_name,
-            query_=query_prepared,
-            parameters_=parameters,
-            routing_=RoutingControl.READ,
-        )
+        routing = RoutingControl.READ
+
+    return {
+        "database_": db_name,
+        "query_": query_prepared,
+        "parameters_": parameters,
+        "routing_": routing,
+    }
 
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    uri: Optional[str] = os.environ.get("URI")
+    db_user: Optional[str] = os.environ.get("DB_USER")
+    db_password: Optional[str] = os.environ.get("DB_PW")
+
     if some(uri) and some(db_user) and some(db_password):
         with GraphDatabase.driver(uri, auth=(db_user, db_password)) as driver:
-            run_query(
-                driver,
+            parameters = prepare_query(
                 "movies",
-                "CREATE (p:Person {name: 'Louis Dupont', born: 2020})",
-                True,
+                "MATCH (p: Person)-[r]-(e2) RETURN p, r, e2 LIMIT $lim",
+                False,
+                {"label": "Person", "lim": 25},
             )
+
+            driver.execute_query(**parameters)
