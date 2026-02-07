24672c0fd0c7203491d50d3a840cd927d6fc9998
LugolBis <ld.desmares@gmail.com>
2026-02-07 16:34:07 +0100

    Improved Neo4j Driver usages

--- src/neo4j_driver.py
+++ src/neo4j_driver.py
@@ -2,31 +2,53 @@ import os
 from typing import Any, LiteralString, Optional
 
 from dotenv import load_dotenv
-from neo4j import GraphDatabase, Query, RoutingControl
+from neo4j import GraphDatabase, Query, Result
+from pandas import DataFrame
 
 from utils import some
 
 
-def prepare_query(
-    db_name: str,
+class Neo4jSession:
+    def __init__(self, uri: str, username: str, password: str, database: str) -> None:
+        self._driver = GraphDatabase.driver(uri, auth=(username, password))
+        self._session = self._driver.session(database=database)
+        self._closed = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        self.close()
+
+    def __del__(self):
+        self.close()
+
+    def close(self) -> None:
+        if not self._closed:
+            if not self._session.closed():
+                self._session.close()
+            self._driver.close()
+
+    def run_query(
+        self,
+        query: LiteralString,
+        parameters: Optional[dict[str, Any]] = None,
+        timeout: Optional[float] = None,
+    ) -> Result:
+        kwargs: dict[str, Any] = _prepare_query(query, parameters, timeout)
+        return self._session.run(**kwargs)
+
+
+def _prepare_query(
     query: LiteralString,
-    write_op: bool,
     parameters: Optional[dict[str, Any]] = None,
     timeout: Optional[float] = None,
 ) -> dict[str, Any]:
     query_prepared = Query(text=query, timeout=timeout)
-    routing: RoutingControl
-
-    if write_op:
-        routing = RoutingControl.WRITE
-    else:
-        routing = RoutingControl.READ
 
     return {
-        "database_": db_name,
-        "query_": query_prepared,
-        "parameters_": parameters,
-        "routing_": routing,
+        "query": query_prepared,
+        "parameters": parameters,
     }
 
 
@@ -38,12 +60,12 @@ if __name__ == "__main__":
     db_password: Optional[str] = os.environ.get("DB_PW")
 
     if some(uri) and some(db_user) and some(db_password):
-        with GraphDatabase.driver(uri, auth=(db_user, db_password)) as driver:
-            parameters = prepare_query(
-                "movies",
-                "MATCH (p: Person)-[r]-(e2) RETURN p, r, e2 LIMIT $lim",
-                False,
-                {"label": "Person", "lim": 25},
+        with Neo4jSession(uri, db_user, db_password, "movies") as session:
+            result: Result = session.run_query(
+                "CREATE (p:Person { born: $born, name: $name }) RETURN p",
+                {"name": "Julius Trevam", "born": 1990},
             )
 
-            driver.execute_query(**parameters)
+            df: DataFrame = result.to_df(expand=True)
+
+        print(df)
