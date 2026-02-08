f93435cee87eea9647a3114c0a105354d81073fa
LugolBis <ld.desmares@gmail.com>
2026-02-08 19:22:15 +0100

    Added documentation

--- src/neo4j_driver.py
+++ src/neo4j_driver.py
@@ -9,21 +9,60 @@ from utils import some
 
 
 class Neo4jSession:
-    def __init__(self, uri: str, username: str, password: str, database: str) -> None:
-        self._driver = GraphDatabase.driver(uri, auth=(username, password))
+    """
+    Neo4jSession is a class to wrap the logic of the Neo4j API to produce more concise code.\n
+    Note : It is hardly recommanded to use this class in a ***with*** clause.
+    """
+
+    def __init__(self, uri: str, user: str, password: str, database: str) -> None:
+        """
+        Init the object with the arguments took in input, who are used to open a `neo4j.Driver` and `neo4j.Session` connection.
+
+        :param self: The object itself.
+        :param uri: URI of the Neo4j instance.
+        :type uri: str
+        :param user: User used to query the database.
+        :type user: str
+        :param password: Password of the `user`.
+        :type password: str
+        :param database: Database name.
+        :type database: str
+        """
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
         self._session = self._driver.session(database=database)
         self._closed = False
 
     def __enter__(self):
+        """
+        Overload the `__enter__` who's provide to use the `Neo4jSession` in ***with*** clause.
+
+        :param self: Description
+        """
         return self
 
     def __exit__(self, exc_type, exc, tb):
+        """
+        Overload the `__enter__` who's provide to use the `Neo4jSession` in ***with*** clause.
+
+        :param self: Description
+        """
         self.close()
 
     def __del__(self):
+        """
+        Automatically close the connection when the object is deleted.\n
+        /!\ Attention : Sometimes it isn't called by python, so i hardly recommand to use the ***with*** clause.
+
+        :param self: Description
+        """
         self.close()
 
     def close(self) -> None:
+        """
+        Call the method `.close()` on both object `neo4j.Driver` and `neo4j.Session`.
+
+        :param self: Description
+        """
         if not self._closed:
             if not self._session.closed():
                 self._session.close()
@@ -35,6 +74,19 @@ class Neo4jSession:
         parameters: Optional[dict[str, Any]] = None,
         timeout: Optional[float] = None,
     ) -> Result:
+        """
+        Get a **Lazy result** of a **Cypher** query with both READ/WRITE rights.
+
+        :param self: The object itself.
+        :param query: Cypher query.
+        :type query: LiteralString
+        :param parameters: Parameters passed to `neo4j.Session.run(parameters=)`
+        :type parameters: Optional[dict[str, Any]]
+        :param timeout: Timeouit before cancel a query.
+        :type timeout: Optional[float]
+        :return: **Lazy result** which could be easily transformed as a **Graph** or a **pandas.DataFrame**.
+        :rtype: Result
+        """
         kwargs: dict[str, Any] = _prepare_query(query, parameters, timeout)
         return self._session.run(**kwargs)
 
@@ -44,6 +96,18 @@ def _prepare_query(
     parameters: Optional[dict[str, Any]] = None,
     timeout: Optional[float] = None,
 ) -> dict[str, Any]:
+    """
+    Format the arguments took in input as the `neo4j.Session.run()` arguments format.
+
+    :param query: Cypher query.
+    :type query: LiteralString
+    :param parameters: Parameters passed to `neo4j.Session.run(parameters=)`
+    :type parameters: Optional[dict[str, Any]]
+    :param timeout:Timeouit before cancel a query.
+    :type timeout: Optional[float]
+    :return: Parameters formated.
+    :rtype: dict[str, Any]
+    """
     query_prepared = Query(text=query, timeout=timeout)
 
     return {
--- src/utils.py
+++ src/utils.py
@@ -4,4 +4,12 @@ T = TypeVar("T")
 
 
 def some(option: Optional[T]) -> TypeGuard[T]:
+    """
+    Provide a concise and safe way to to check the `option` took in input isn't a 'None' value.
+
+    :param option: An optional value, which can be a `None` or an object of type `T`
+    :type option: Optional[T]
+    :return: A wrapped boolean who's bring the power of `mypy`
+    :rtype: TypeGuard[T]
+    """
     return option is not None
