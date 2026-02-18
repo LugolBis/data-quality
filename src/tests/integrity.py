6e00d0f0ae70c9c2e9e906bc3f16a08bcff4a19b
tianyi.yang <thierry.yang2002@gmail.com>
2026-02-18 22:39:06 +0100

    Relationship similarity tests passed

--- src/tests/integrity.py
+++ src/tests/integrity.py
@@ -5,6 +5,7 @@ from quality.integrity import (
     distr_nodes_properties,
     distr_relationships_properties,
     detecter_doublons_node,
+    detecter_doublons_relationships,
 )
 from quality.types import EntityStats, TextSimilarity
 from utils.utils import some
@@ -12,18 +13,28 @@ from utils.utils import some
 
 def main(session: Neo4jSession) -> None:
     properties: Optional[list[EntityStats]] = distr_nodes_properties(session)
-    similarities: Optional[list[TextSimilarity]] = detecter_doublons_node(
-        session, seuil_similarite=0.6
+
+    similarities_node: Optional[list[TextSimilarity]] = detecter_doublons_node(
+        session, seuil_similarite=0.9
     )
+    
     relationships: Optional[list[EntityStats]] = distr_relationships_properties(session)
+
+    similarities_relationships: Optional[list[TextSimilarity]] = detecter_doublons_relationships(
+        session, seuil_similarite=0.9
+    )
 
     if some(properties):
         print(properties)
 
-    if some(similarities):
+    if some(similarities_node):
         print("\n")
-        print("\n".join([sim.__repr__() for sim in similarities]))
+        print("\n".join([sim.__repr__() for sim in similarities_node]))
 
     if some(relationships):
         print("\n")
         print(relationships)
+
+    if some(similarities_relationships):
+        print("\n")
+        print("\n".join([sim.__repr__() for sim in similarities_relationships]))
