27b5d93822c59d209c47bef639837dda943d0254
LugolBis <ld.desmares@gmail.com>
2026-02-21 21:00:33 +0100

    Finished to implement integrity page

--- src/ui/pages/integrity.py
+++ src/ui/pages/integrity.py
@@ -1,11 +1,24 @@
+from typing import Any, Callable
+
+import numpy as np
 import streamlit as st
 
-from quality.integrity import distr_nodes_properties, distr_relationships_properties
-from ui.components import _static_analysis
+from quality.integrity import (
+    detecter_doublons_node,
+    detecter_doublons_relationships,
+    distr_nodes_properties,
+    distr_relationships_properties,
+)
+from ui.components import _dynamic_analysis, _static_analysis
+from ui.utils import _lazy_render
 
 
 def render() -> None:
     _headers()
+    st.divider()
+    _nodes_duplicates()
+    st.divider()
+    _relationships_duplicates()
     st.divider()
     _nodes_properties()
     st.divider()
@@ -15,6 +28,46 @@ def render() -> None:
 def _headers() -> None:
     st.title("Integrity")
     st.markdown("#### Analysis of the integrity of the database.")
+
+
+def _nodes_duplicates() -> None:
+    lazy_render: Callable[[], Any] = _lazy_render(
+        st.select_slider,
+        label="Select nodes similarity threshold :",
+        options=np.arange(0, 1.05, 0.05).tolist(),
+        value=0.5,
+        key="_integrity_nodes_duplicates_treshold",
+    )
+
+    _dynamic_analysis(
+        "#### Detection of duplicate **Nodes**.",
+        "Scan all nodes to find potential duplicates based on string property similarity using SequenceMatcher.",
+        "Analyse nodes duplicates",
+        lazy_renders=[lazy_render],
+        func=detecter_doublons_node,
+        lazy_func_args={"seuil_similarite": "_integrity_nodes_duplicates_treshold"},
+    )
+
+
+def _relationships_duplicates() -> None:
+    lazy_render: Callable[[], Any] = _lazy_render(
+        st.select_slider,
+        label="Select relationships similarity threshold :",
+        options=np.arange(0, 1.05, 0.05).tolist(),
+        value=0.8,
+        key="_integrity_relationships_duplicates_treshold",
+    )
+
+    _dynamic_analysis(
+        "#### Detection of duplicate **Relationships**.",
+        "Scan all relationships to find potential duplicates based on string property similarity using SequenceMatcher.",
+        "Analyse relationships duplicates",
+        lazy_renders=[lazy_render],
+        func=detecter_doublons_relationships,
+        lazy_func_args={
+            "seuil_similarite": "_integrity_relationships_duplicates_treshold"
+        },
+    )
 
 
 def _nodes_properties() -> None:
