2b0dcafe9aa3ea54248a01c91c8208c5cc2a47f2
LugolBis <ld.desmares@gmail.com>
2026-02-22 20:24:42 +0100

    Improved LoadResult enumeration

--- src/load/enums.py
+++ src/load/enums.py
@@ -15,10 +15,10 @@ class LoadResult(Enum):
     Enumeration to describe the Load Result.
     """
 
-    NO_DB_HOME = "failed to get the Neo4j instance home folder"
-    LOAD_SUCCESS = "successfully to load the data"
-    LOAD_FAILED = "failed to load the data"
-    STOP_FAILED = "failed to stop the instance to import the data"
-    START_FAILED = "failed to restart the database after the import"
-    RECOVERY_SUCCESS = "successfully recovery the database after the failed import"
-    RECOVERY_FAILED = "failed to recovery the database"
+    NO_DB_HOME = "Failed to get the Neo4j instance home folder."
+    LOAD_SUCCESS = "Successfully to load the data."
+    LOAD_FAILED = "Failed to load the data."
+    STOP_FAILED = "Failed to stop the instance to import the data."
+    START_FAILED = "Failed to restart the database after the import."
+    RECOVERY_SUCCESS = "Successfully recovery the database after the failed import."
+    RECOVERY_FAILED = "Failed to recovery the database."
