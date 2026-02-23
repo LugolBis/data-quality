e4704448e6c123db7211ab5a8a8ca4d7b2764b64
LugolBis <ld.desmares@gmail.com>
2026-02-23 16:53:58 +0100

    Improved UI navigation

--- src/ui/layout.py
+++ src/ui/layout.py
@@ -15,20 +15,24 @@ from ui.utils import _config_page
 
 
 def main() -> None:
-    pages: list[Any]
+    pages: list[Any] | dict[str, Any]
 
     if not st.session_state.get("is_connected", False):
         pages = [st.Page(**_config_page(log_in.render))]
     else:
-        pages = [
-            st.Page(**_config_page(log_in.render)),
-            st.Page(**_config_page(load_data.render)),
-            st.Page(**_config_page(consistency.render)),
-            st.Page(**_config_page(integrity.render)),
-            st.Page(**_config_page(lisibility.render)),
-            st.Page(**_config_page(outlier.render)),
-            st.Page(**_config_page(property_schema.render)),
-        ]
+        pages = {
+            "Database management": [
+                st.Page(**_config_page(log_in.render)),
+                st.Page(**_config_page(load_data.render)),
+            ],
+            "Data Quality analysis": [
+                st.Page(**_config_page(consistency.render)),
+                st.Page(**_config_page(integrity.render)),
+                st.Page(**_config_page(lisibility.render)),
+                st.Page(**_config_page(outlier.render)),
+                st.Page(**_config_page(property_schema.render)),
+            ],
+        }
 
     pg = st.navigation(pages)
     pg.run()
