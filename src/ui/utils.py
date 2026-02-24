7cdaecbbc92f9a517be562c74847d2d44ed19c6f
LugolBis <ld.desmares@gmail.com>
2026-02-24 13:56:31 +0100

    Added a lazy function for analysis call

--- src/ui/utils.py
+++ src/ui/utils.py
@@ -1,6 +1,9 @@
 from functools import partial
 from typing import Any, Callable
 
+from driver.neo4j_driver import Neo4jSession
+from ui.components import _button_call
+
 
 def _config_page(function: Callable[..., None]) -> dict[str, Any]:
     """
@@ -36,3 +39,37 @@ def _lazy_render(call: Callable[..., Any], *args, **kwargs) -> Callable[[], Any]
     :rtype: Callable[[], Any]
     """
     return partial(call, *args, **kwargs)
+
+
+def _lazy_analyze(
+    key: str,
+    func: Callable[..., Any],
+    session: Neo4jSession,
+    func_args: dict[str, Any],
+    progress_message: str,
+) -> Callable[[], None]:
+    """
+    Wrap an analyze function who's called by buttons.
+
+    :param key: Key state of the button call.
+    :type key: str
+    :param func: Analyze function.
+    :type func: Callable[..., Any]
+    :param session: A `Neo4j` session to query the database.
+    :type session: Neo4jSession
+    :param func_args: Arguments to be passed to the analyze function.
+    :type func_args: dict[str, Any]
+    :param progress_message: Message to be display during the analyzing.
+    :type progress_message: str
+    :return: A lazy function.
+    :rtype: Callable[[], None]
+    """
+
+    return partial(
+        _button_call,
+        key,
+        func,
+        session,
+        func_args,
+        progress_message,
+    )
