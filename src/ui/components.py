d7d695ca479e06a5afc2c53bd1e1cd26bedd5011
LugolBis <ld.desmares@gmail.com>
2026-02-28 18:34:41 +0100

    Added components for quality score

--- src/ui/components.py
+++ src/ui/components.py
@@ -1,13 +1,17 @@
-from typing import Any, Callable, Optional
+from typing import TYPE_CHECKING, Any
 
 import pandas as pd
 import streamlit as st
 from streamlit import session_state as app_st
 
-from driver.neo4j_driver import Neo4jSession
 from quality.utils import _to_dataframe
 from ui.enums import WidgetState
 from utils.utils import some
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from driver.neo4j_driver import Neo4jSession
 
 
 def _static_analysis(
@@ -27,7 +31,8 @@ def _static_analysis(
     :type func: Callable[..., Optional[Any]]
     :param func_args: If necessary arguments to the function.
     :type func_args: dict[str, Any] | None
-    :param flatten: List of properties of the result of `func` who needs to be flatten to be converted as a `pandas.DataFrame`.
+    :param flatten: List of properties of the result of `func` who needs to be flatten
+     to be converted as a `pandas.DataFrame`.
     :type flatten: list[str] | None
     :param button_label: Text to be display on the button.
     :type button_label: str
@@ -38,7 +43,7 @@ def _static_analysis(
 
     _button(button_label, key)
 
-    _display_df(key)
+    _display_df(f"{key}_res")
 
 
 def _dynamic_analysis(
@@ -64,9 +69,12 @@ def _dynamic_analysis(
     :type func: Callable[..., Optional[Any]]
     :param func_args: If necessary arguments to the function.
     :type func_args: dict[str, Any] | None
-    :param lazy_func_args: Dict of lazy arguments who need to retrieved with `app_st[value]`. The keys needs to match the arguments of `func` and the value are used to retrieve them from the session state.
+    :param lazy_func_args: Dict of lazy arguments who need to retrieved with
+     `app_st[value]`. The keys needs to match the arguments of `func` and the value are
+      used to retrieve them from the session state.
     :type lazy_func_args: dict[str, str] | None
-    :param flatten: List of properties of the result of `func` who needs to be flatten to be converted as a `pandas.DataFrame`.
+    :param flatten: List of properties of the result of `func` who needs to be flatten
+     to be converted as a `pandas.DataFrame`.
     :type flatten: list[str] | None
     :param button_label: Text to be display on the button.
     :type button_label: str
@@ -83,24 +91,46 @@ def _dynamic_analysis(
 
     _button(button_label, key)
 
-    _display_df(key)
+    _display_df(f"{key}_res")
 
 
-def _analyze_call(
+def _static_score(
+    key: str,
+    button_label: str = "Refresh",
+) -> None:
+    key_score: str = f"{key}_score"
+    if key_score in app_st:
+        app_st[key_score]()
+
+    key_score_res: str = f"{key_score}_res"
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        _display_score(key_score_res)
+
+    with col2:
+        _button(
+            button_label,
+            key_score,
+        )
+
+
+def _analyze_call(  # noqa: PLR0913
     func: Callable[..., Any],
     key: str,
-    func_args: Optional[dict[str, Any]] = None,
-    lazy_func_args: Optional[dict[str, str]] = None,
-    flatten: Optional[list[str]] = None,
-    to_df: bool = True,
+    func_args: dict[str, Any] | None = None,
+    lazy_func_args: dict[str, str] | None = None,
+    flatten: list[str] | None = None,
+    to_df: bool = True,  # noqa: FBT001, FBT002
     progress_message: str = "Analysis in progress...",
 ) -> None:
     # TODO ! Replace 'key' by 'key_res'
     key_res = f"{key}_res"
 
-    func_args = dict(func_args) if func_args else {}
-    lazy_func_args = dict(lazy_func_args) if lazy_func_args else {}
-    flatten = list(flatten) if flatten else []
+    func_args = func_args if func_args else {}
+    lazy_func_args = lazy_func_args if lazy_func_args else {}
+    flatten = flatten if flatten else []
 
     try:
         with st.spinner(progress_message):
@@ -121,7 +151,10 @@ def _analyze_call(
                 else:
                     app_st[key_res] = {
                         "state": WidgetState.ERROR,
-                        "data": "Failed to convert the result as pandas.DataFrame during _analyze_call().",
+                        "data": (
+                            "Failed to convert the result as pandas.DataFrame during"
+                            " _analyze_call()."
+                        ),
                     }
             else:
                 app_st[key_res] = {"state": WidgetState.SUCCESS, "data": results}
@@ -131,10 +164,38 @@ def _analyze_call(
         app_st[key_res] = {"state": WidgetState.ERROR, "data": error}
 
 
+def _score_call(
+    func: Callable[..., Any],
+    key: str,
+    func_args: dict[str, Any] | None = None,
+    lazy_func_args: dict[str, str] | None = None,
+    progress_message: str = "Compute Quality score...",
+) -> None:
+    func_args = dict(func_args) if func_args else {}
+    lazy_func_args = dict(lazy_func_args) if lazy_func_args else {}
+
+    key_score_res: str = f"{key}_score_res"
+
+    try:
+        with st.spinner(progress_message):
+            for k, v in lazy_func_args.items():
+                if v in app_st:
+                    if v.endswith("_res"):
+                        func_args[k] = app_st[v]["data"]
+                    else:
+                        func_args[k] = app_st[v]
+
+            score = func(**func_args)
+
+        app_st[key_score_res] = {"state": WidgetState.SUCCESS, "data": score}
+    except Exception as error:
+        app_st[key_score_res] = {"state": WidgetState.ERROR, "data": error}
+
+
 def _spinner_call(
     func: Callable[..., Any],
     key_res: str,
-    func_args: dict[str, Any] = dict(),
+    func_args: dict[str, Any] = {},
     progress_message: str = "Work in progress...",
 ) -> None:
     try:
@@ -168,10 +229,8 @@ def _button(
         app_st[key]()
 
 
-def _display_df(key: str) -> None:
+def _display_df(key_res: str) -> None:
     # Persistent display
-    key_res = f"{key}_res"
-
     match app_st[key_res]["state"]:
         case WidgetState.ERROR:
             st.exception(app_st[key_res]["data"])
@@ -183,7 +242,36 @@ def _display_df(key: str) -> None:
                 st.dataframe(app_st[key_res]["data"], use_container_width=True)
             else:
                 st.error(
-                    f"You can't display a pandas.DataFrame from an object of {type(app_st[key_res]['data'])}"
+                    (
+                        "You can't display a pandas.DataFrame from an object of"
+                        f" {type(app_st[key_res]['data'])}"
+                    ),
+                )
+        case _:
+            pass
+
+
+def _display_score(key_res: str) -> None:
+    match app_st[key_res]["state"]:
+        case WidgetState.ERROR:
+            st.exception(app_st[key_res]["data"])
+        case WidgetState.EMPTY:
+            st.success("There isn't any data detected.")
+        case WidgetState.SUCCESS:
+            data = app_st[key_res]["data"]
+            if isinstance(data, float):
+                st.metric(
+                    label="Quality score :",
+                    value=data,
+                    format="percent",
+                    border=True,
+                )
+            else:
+                st.error(
+                    (
+                        "You can't display a quality score from an object of"
+                        f" {type(data)}"
+                    ),
                 )
         case _:
             pass
