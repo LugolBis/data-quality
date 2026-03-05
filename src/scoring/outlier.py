574c03ec98c8024e4329bee6624c50d0b490afd6
tianyi.yang <thierry.yang2002@gmail.com>
2026-03-05 18:47:55 +0100

    Fix outlier score problem

--- src/scoring/outlier.py
+++ src/scoring/outlier.py
@@ -11,39 +11,32 @@ def numerical_outlier_ratio(
     df_cached: pd.DataFrame,
     group_by: str = "label",
     population: str = "count",
-    outlier_count: str = "outlier_count",
 ) -> float:
-    """
-    Compute a Weighted score based on the ratio of numerical outliers.
-
-    :param df: Data containing detected outliers per label and property.
-    :type df: Optional[pd.DataFrame]
-    :param df_cached: Cached population data containing total element count per label.
-    :type df_cached: pd.DataFrame
-    :param group_by: Column to be used to group by on (e.g., 'label').
-    :type group_by: str
-    :param population: Column in `df_cached` representing the total number of elements.
-    :type population: str
-    :param outlier_count: Column in `df` representing the number of detected outliers.
-    :type outlier_count: str
-    :return: A quality score between `0.0` and `1.0`.
-    :rtype: float
-    """
     if df is None or df.empty:
         return 1.00
 
-    invalid: pd.Series[Any] = df.groupby(group_by)[outlier_count].sum()
+    invalid: pd.Series[Any] = df.groupby(group_by).size()
 
-    total_pop: pd.Series[Any] = df_cached[population].copy()
-    total_pop.index = df_cached[group_by]
+    df_cached_patched = df_cached.copy()
+    if group_by not in df_cached_patched.index.names:
+        df_cached_patched.set_index(group_by, inplace=True, drop=False)
 
-    percent: pd.Series[Any] = (invalid / total_pop).fillna(0.00).clip(upper=1.0)
+    total_pop: pd.Series[Any] = df_cached_patched[population].copy()
+
+    missing_labels = invalid.index.difference(total_pop.index)
+    if not missing_labels.empty:
+        for lbl in missing_labels:
+            df_cached_patched.loc[lbl] = {group_by: lbl, population: invalid[lbl]}
+
+        total_pop = df_cached_patched[population]
+
+    percent: pd.Series[Any] = invalid.reindex(total_pop.index).fillna(0) / total_pop
+    percent = percent.clip(upper=1.0)
 
     inverted_score = _weighted(
-        df_cached,
-        score_label=percent,
-        group_by=group_by,
-        population=population,
+        df_cached_patched, score_label=percent, group_by=group_by, population=population
     )
 
-    return round(1.0 - inverted_score, 2)
+    final_score = round(1.0 - inverted_score, 2)
+
+    return final_score
